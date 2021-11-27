319e688fe06df89fe2bc1da9191d9820dd8d3101
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-11-28 06:15:20 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -1,39 +1,56 @@
 {
    "files": [   
-
-{
-         "name": "Dtac-Lazada-FREE1",
-         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_05.ovpn"
-},
-
-{
-         "name": "Dtac-Lazada-FREE2",
-         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_06.ovpn"
-},
 {
        "name": "Dtac-HappyWork-FREE1",
          "url": "https://pastebin.com/raw/PsQHmSHf"
 },
-
- {      "name": "Dtac-HappyWork-FREE2",
-          "url": "https://pastebin.com/raw/aF3JuPNU"
- },
-            
-             {
+{
+       "name": "Dtac-HappyWork-VIP1",
+         "url": "https://pastebin.com/raw/Wys5zr8p"
+},
+{
+       "name": "Dtac-HappyWork-VIP2",
+         "url": "https://pastebin.com/raw/Rf4SMWDW"
+},
+{
+         "name": "Dtac-Lazada-FREE1",
+         "url": "https://pastebin.com/raw/84gvyBMn"
+},
+{
+       "name": "Dtac-Lazada-FREE2",
+         "url": "https://pastebin.com/raw/PJ7mtsnL"
+},
+{
+         "name": "Dtac-Lazada-VIP",
+         "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-vip.ovpn"
+},
+{
        "name": "Dtac-Line12Apps-FREE1",
+         "url": "https://pastebin.com/raw/8spqiBGJ"
+      },
+{
+       "name": "Dtac-Line12Apps-FREE2",
          "url": "https://kguza.net/web/file/maine/Dtac_Line_05.ovpn"
       },
-                   {
-       "name": "Dtac-Line12Apps-FREE2",
-         "url": "https://kguza.net/web/file/maine/Dtac_Line_06.ovpn"
+{
+       "name": "Dtac-Line12Apps-VIP1",
+         "url": "https://pastebin.com/raw/6SNdaCQi"
       },
-           {
-       "name": "Dtac-Line12Apps-VIP",
+{
+       "name": "Dtac-Line12Apps-VIP2",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-vip.ovpn"
+      },
+{
+       "name": "True-NoPro-FREE1",
+         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-vip.ovpn"
+      },
+{
+       "name": "True-NoPro-FREE2",
+         "url": "https://pastebin.com/raw/tPYtSgC1"
       }
       
  ],
-"message":"DTAC-Lazada\nDTAC-HappyWork\nDTAC-Social\nDTAC-Line12Apps\nTRUE-NoPro\nFast connection⚡\n\n Thank you for using😊\n",
+"message":"Added VIP-VPS Servers\n\nDTAC-Lazada\nDTAC-HappyWork\nDTAC-Line12Apps\nTRUE-NoPro\nFast connection⚡\n\n Thank you for using😊\n",
    "title":"New config update available",
-   "version": 34
+   "version": 35
 }
