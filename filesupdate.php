c01b310df341693fc858d31bc10690767c3b3e4e
nds999 <93828042+nds999@users.noreply.github.com>
2021-11-27 19:00:34 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -3,84 +3,33 @@
 
 {
          "name": "Dtac-Lazada-FREE1",
-         "url": "https://nds999.000webhostapp.com/01.LAZADA.ovpn"
+         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_01.ovpn"
 },
 
 {
          "name": "Dtac-Lazada-FREE2",
-         "url": "https://nds999.000webhostapp.com/02.LAZADA.ovpn"
-},
-
-{
-         "name": "Dtac-Lazada-FREE3",
-         "url": "https://nds999.000webhostapp.com/03.LAZADA.ovpn"
-},
-
-{
-         "name": "Dtac-Lazada-FREE4",
-         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_05.ovpn"
-},
-
-{
-         "name": "Dtac-Lazada-FREE5",
-         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_06.ovpn"
+         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_02.ovpn"
 },
 {
        "name": "Dtac-HappyWork-FREE1",
          "url": "https://pastebin.com/raw/PsQHmSHf"
 },
-{
-       "name": "Dtac-HappyWork-FREE2",
-         "url": "https://nds999.000webhostapp.com/True01.ovpn"
-},
 
-{      "name": "Dtac-HappyWork-FREE3",
-         "url": "https://nds999.000webhostapp.com/05.Dtac-HappyWork.ovpn"
-},
-
-{      "name": "Dtac-HappyWork-FREE4",
-          "url": "https://nds999.000webhostapp.com/hw1.ovpn"
- },
- {      "name": "Dtac-HappyWork-FREE5",
+ {      "name": "Dtac-HappyWork-FREE2",
           "url": "https://pastebin.com/raw/aF3JuPNU"
  },
+            
              {
        "name": "Dtac-Line12Apps-FREE1",
-         "url": "https://nds999.000webhostapp.com/01.LINE-12Apps.ovpn"
-      },
-             {
-       "name": "Dtac-Line12Apps-FREE2",
-         "url": "https://nds999.000webhostapp.com/02-LINE-12Apps.ovpn"
-      },
-             {
-       "name": "Dtac-Line12Apps-FREE3",
          "url": "https://kguza.net/web/file/maine/Dtac_Line_05.ovpn"
       },
                    {
-       "name": "Dtac-Line12Apps-FREE4",
+       "name": "Dtac-Line12Apps-FREE2",
          "url": "https://kguza.net/web/file/maine/Dtac_Line_06.ovpn"
       },
            {
        "name": "Dtac-Line12Apps-VIP",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-vip.ovpn"
-      },
-       {
-       "name": "Dtac-Social-FREE1",
-         "url": "https://nds999.000webhostapp.com/Dtac-Social.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE1",
-         "url": "https://nds999.000webhostapp.com/True03.ovpn"
-},
-
-{
-       "name": "True-NoPro-FREE2",
-         "url": "https://nds999.000webhostapp.com/07.True-NoPro-FREE.ovpn"
-         },
-      
-      {
-       "name": "True-NoPro-FREE3",
-         "url": "https://nds999.000webhostapp.com/True02.ovpn"
       }
       
  ],
