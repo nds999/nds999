6d5ba950f397947040275338e83243adf1578301
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-11-28 16:44:14 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -5,12 +5,12 @@
          "url": "https://pastebin.com/raw/PsQHmSHf"
 },
 {
-       "name": "Dtac-HappyWork-VIP1",
-         "url": "https://pastebin.com/raw/Wys5zr8p"
+       "name": "Dtac-HappyWork-FREE2",
+         "url": "https://pastebin.com/raw/Rf4SMWDW"
 },
 {
-       "name": "Dtac-HappyWork-VIP2",
-         "url": "https://pastebin.com/raw/Rf4SMWDW"
+       "name": "Dtac-HappyWork-VIP1",
+         "url": "https://pastebin.com/raw/Wys5zr8p"
 },
 {
          "name": "Dtac-Lazada-FREE1",
@@ -21,36 +21,24 @@
          "url": "https://pastebin.com/raw/PJ7mtsnL"
 },
 {
-         "name": "Dtac-Lazada-VIP",
+         "name": "Dtac-Lazada-FREE3",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-vip.ovpn"
 },
 {
        "name": "Dtac-Line12Apps-FREE1",
-         "url": "https://pastebin.com/raw/8spqiBGJ"
-      },
-{
-       "name": "Dtac-Line12Apps-FREE2",
          "url": "https://kguza.net/web/file/maine/Dtac_Line_05.ovpn"
       },
 {
        "name": "Dtac-Line12Apps-VIP1",
-         "url": "https://pastebin.com/raw/6SNdaCQi"
-      },
-{
-       "name": "Dtac-Line12Apps-VIP2",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-vip.ovpn"
       },
 {
        "name": "True-NoPro-FREE1",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-vip.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE2",
          "url": "https://pastebin.com/raw/tPYtSgC1"
       }
       
  ],
 "message":"Added VIP-VPS Servers\n\nDTAC-Lazada\nDTAC-HappyWork\nDTAC-Line12Apps\nTRUE-NoPro\nFast connection⚡\n\n Thank you for using😊\n",
    "title":"New config update available",
-   "version": 35
+   "version": 36
 }
