74fd0af7ec24a1a19d295bf0dc70790fa6f4b8bb
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-10 04:39:21 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -33,10 +33,6 @@
          "url": "https://kguza.net/web/file/maine/Dtac_Line_05.ovpn"
       },
 {
-       "name": "Dtac-Line12Apps-FREE2",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-free2.ovpn"
-      },
-{
        "name": "Dtac-Line12Apps-FREE3",
          "url": "https://pastebin.com/raw/DvKMHKR8"
       },
@@ -64,5 +60,5 @@
  ],
 "message":"DTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nDTAC-Social\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 52
+   "version": 53
 }
