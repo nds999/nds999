a8b3cf71f954fa0676e0248517845c45b8e558a3
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-13 22:31:16 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -64,5 +64,5 @@
  ],
 "message":"VIP 30day 40baht\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nDTAC-Social\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 57
+   "version": 58
 }
