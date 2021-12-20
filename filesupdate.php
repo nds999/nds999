27f2b35cccc9f9084f1a3dee4d8ccd47a6065813
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-21 05:18:46 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -9,20 +9,12 @@
          "url": "https://raw.githubusercontent.com/nds999/nds999/FNET-VPN/hw2.ovpn"
 },
 {
-       "name": "Dtac-HappyWork-VIP",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/hw-vip1.ovpn"
-},
-{
          "name": "Dtac-Lazada-FREE1",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-vip.ovpn"
 },
 {
          "name": "Dtac-Lazada-FREE2",
          "url": "https://kguza.net/web/file/maine/Dtac_Lazada_05.ovpn"
-},
-{
-       "name": "Dtac-Lazada-VIP",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-vip1.ovpn"
 },
 {
        "name": "Dtac-Line12Apps-FREE1",
@@ -32,14 +24,6 @@
        "name": "Dtac-Line12Apps-FREE2",
          "url": "https://kguza.net/web/file/maine/Dtac_Line_06.ovpn"
       },
-{
-       "name": "Dtac-Line12Apps-FREE3",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/us.line12apps.free3.ovpn"
-      },
-      {
-       "name": "Dtac-Line12Apps-VIP",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12app.vps.vip.ovpn"
-},
 {
        "name": "Dtac-Social-FREE1",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/social-free1ovpn"
@@ -51,14 +35,9 @@
 {
        "name": "True-NoPro-FREE2",
          "url": "https://kguza.net/web/file/maine/True_06.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE3",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/true-nopro-free3.ovpn"
-}
-      
+      }
  ],
-"message":"VIP 1Month 40baht\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nDTAC-Social\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
+"message":"DTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nDTAC-Social\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 60
+   "version": 61
 }
