4cd07ca56164b0c4302ca87eb1dd8fbcaddf7943
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-23 16:34:01 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -1,51 +1,23 @@
 {
    "files": [   
 {
-       "name": "Dtac-HappyWork-FREE1",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/hw-free1.ovpn"
+       "name": "Dtac-HappyWork-VPS-VIP",
+         "url": "https://tak65.000webhostapp.com/happywork-vip1.ovpn"
 },
 {
-       "name": "Dtac-HappyWork-FREE2",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/FNET-VPN/hw2.ovpn"
+         "name": "Dtac-Lazada-VPS-VIP",
+         "url": "https://tak65.000webhostapp.com/lazada-vip1.ovpn"
 },
 {
-       "name": "Dtac-HappyWork-FREE3",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/hw3.ovpn"
-},
-{
-         "name": "Dtac-Lazada-FREE1",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-vip.ovpn"
-},
-{
-         "name": "Dtac-Lazada-FREE2",
-         "url": "https://kguza.net/web/file/maine/Dtac_Lazada_05.ovpn"
-},
-{
-       "name": "Dtac-Line12Apps-FREE1",
-         "url": "https://kguza.net/web/file/maine/Dtac_Line_05.ovpn"
-      },
-{
-       "name": "Dtac-Line12Apps-FREE2",
+       "name": "Dtac-Line12Apps-VPS-VIP",
          "url": "https://kguza.net/web/file/maine/Dtac_Line_06.ovpn"
       },
 {
-       "name": "Dtac-Line12Apps-FREE3",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12apps-vps3.ovpn"
-      },
-{
-       "name": "Dtac-Social-FREE1",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/social-free1ovpn"
-      },
-{
-       "name": "True-NoPro-FREE1",
-         "url": "https://kguza.net/web/file/maine/True_05.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE2",
+       "name": "True-NoPro-VPS-VIP",
          "url": "https://kguza.net/web/file/maine/True_06.ovpn"
       }
  ],
-"message":"DTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nDTAC-Social\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
+"message":"VPS -VIP SERVERS\n\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
    "version": 62
 }
