c259babe6b17453453112c1cbbe9b04abe45020c
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-24 04:31:01 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -1,8 +1,12 @@
 {
    "files": [   
 {
-       "name": "Dtac-HappyWork-VPS-VIP",
+       "name": "Dtac-HappyWork-VPS-VIP1",
          "url": "https://tak65.000webhostapp.com/happywork-vip1.ovpn"
+},
+{
+       "name": "Dtac-HappyWork-VPS-VIP2",
+         "url": "https://raw.githubusercontent.com/nds999/nds999/main/hw-free1.ovpn"
 },
 {
          "name": "Dtac-Lazada-VPS-VIP1",
@@ -31,5 +35,5 @@
  ],
 "message":"VPS -VIP SERVERS\n\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 63
+   "version": 64
 }
