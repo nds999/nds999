8eabb736c59b28a0501f802445dd07fbb76d32c8
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-24 04:27:33 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -5,16 +5,28 @@
          "url": "https://tak65.000webhostapp.com/happywork-vip1.ovpn"
 },
 {
-         "name": "Dtac-Lazada-VPS-VIP",
+         "name": "Dtac-Lazada-VPS-VIP1",
          "url": "https://tak65.000webhostapp.com/lazada-vip1.ovpn"
 },
 {
-       "name": "Dtac-Line12Apps-VPS-VIP",
+         "name": "Dtac-Lazada-VPS-VIP2",
+         "url": "https://kguza.net/web/file/maine/Dtac..Lazada..07.ovpn"
+},
+{
+       "name": "Dtac-Line12Apps-VPS-VIP1",
          "url": "https://tak65.000webhostapp.com/line12apps-vip1.ovpn"
       },
 {
-       "name": "True-NoPro-VPS-VIP",
+       "name": "Dtac-Line12Apps-VPS-VIP2",
+         "url": "https://kguza.net/web/file/maine/Dtac..Line..07.ovpn"
+      },
+{
+       "name": "True-NoPro-VPS-VIP1",
          "url": "https://tak65.000webhostapp.com/true-free1.ovpn"
+      },
+{
+       "name": "True-NoPro-VPS-VIP2",
+         "url": "https://kguza.net/web/file/maine/True..07.ovpn"
       }
  ],
 "message":"VPS -VIP SERVERS\n\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
