6361bf35b63d77a7b1ba88a61ec99fb0ed546b81
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2021-12-24 18:50:19 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -1,39 +1,39 @@
 {
    "files": [   
 {
-       "name": "Dtac-HappyWork-VPS-VIP1",
+       "name": "Dtac-HappyWork-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/happywork-vip1.ovpn"
 },
 {
-       "name": "Dtac-HappyWork-VPS-VIP2",
+       "name": "Dtac-HappyWork-FREE",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/hw-free1.ovpn"
 },
 {
-         "name": "Dtac-Lazada-VPS-VIP1",
+         "name": "Dtac-Lazada-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/lazada-vip1.ovpn"
 },
 {
-         "name": "Dtac-Lazada-VPS-VIP2",
+         "name": "Dtac-Lazada-FREE",
          "url": "https://kguza.net/web/file/maine/Dtac..Lazada..07.ovpn"
 },
 {
-       "name": "Dtac-Line12Apps-VPS-VIP1",
+       "name": "Dtac-Line12Apps-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/line12apps-vip1.ovpn"
       },
 {
-       "name": "Dtac-Line12Apps-VPS-VIP2",
+       "name": "Dtac-Line12Apps-FREE",
          "url": "https://kguza.net/web/file/maine/Dtac..Line..07.ovpn"
       },
 {
-       "name": "True-NoPro-VPS-VIP1",
+       "name": "True-NoPro-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/true-free1.ovpn"
       },
 {
-       "name": "True-NoPro-VPS-VIP2",
+       "name": "True-NoPro-FREE",
          "url": "https://kguza.net/web/file/maine/True..07.ovpn"
       }
  ],
-"message":"VPS -VIP SERVERS\n\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
+"message":"VPS -VIP Server 30Days - 30Baht\n\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 64
+   "version": 65
 }
