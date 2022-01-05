8e7e337867f73ecfe75439b1f2ffc962085780c1
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-01-05 13:35:04 +0700

    Update appupdate.php

--- appupdate.php
+++ appupdate.php
@@ -1,5 +1,5 @@
 {
- "versionCode": "1.1.11",
+ "versionCode": "1.1.10",
   "url":   "https://download1075.mediafire.com/n800mwtldoig/hrewed31cvgqlwr/SECURE+LITE+VPN.apk",
    "Message": "-•NEW UPDATE AVAILABLE•!!!\n\n🆕WHAT'S NEW???\n\nSECURE LITE VPN\n\n1️⃣FIXED ALL BUGS☑️\n\nBY: CJTECH CHANNEL.💚"
    }\n\n
