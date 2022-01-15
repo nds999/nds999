6993b8e8dd48d5e3e5c7733a02d061d73c2f0e0d
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-01-15 17:58:10 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -25,8 +25,36 @@
        "name": "SG-Dtac-Line12Apps-FREE1",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12app2.ovpn"
       },
+{ 
+"name": "SG-Dtac-NoPro-FREE1", 
+"url": "https://kguza.net/web/file/maine/Dtac..01.ovpn" 
+}, 
+{ 
+"name": "SG-Dtac-NoPro-FREE2", 
+"url": "https://kguza.net/web/file/maine/Dtac..02.ovpn" 
+}, 
+{ 
+"name": "SG-Dtac-NoPro-FREE3", 
+"url": "https://kguza.net/web/file/maine/Dtac..03.ovpn" 
+}, 
+{ 
+"name": "SG-Dtac-NoPro-FREE4", 
+"url": "https://kguza.net/web/file/maine/Dtac..04.ovpn" 
+}, 
+{ 
+"name": "SG-Dtac-NoPro-FREE5", 
+"url": "https://kguza.net/web/file/maine/Dtac..05.ovpn" 
+}, 
+{ 
+"name": "SG-Dtac-NoPro-FREE6", 
+"url": "https://kguza.net/web/file/maine/Dtac..06.ovpn" 
+}, 
+{ 
+"name": "SG-Dtac-NoPro-FREE7", 
+"url": "https://kguza.net/web/file/maine/Dtac..07.ovpn" 
+},
 {
-       "name": "SG-Dtac-NoPro-FREE1",
+       "name": "SG-Dtac-NoPro-FREE8",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro2ovpn"
       },
 {
@@ -48,5 +76,5 @@
  ],
 "message":"Includes free new vps servers\n\nNew added DTAC-NoPro\nDTAC-HappyWork\nDTAC-Lazada\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 80
+   "version": 81
 }
