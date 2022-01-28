68b1759c976717c94e9998e16b8c5b826e7e96c7
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-01-28 20:52:35 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -1,14 +1,6 @@
 {
    "files": [   
 {
-       "name": "Dtac-HappyWork-VPS-VIP",
-         "url": "https://tak65.000webhostapp.com/happywork-vip1.ovpn"
-},
-{
-       "name": "Dtac-HappyWork-FREE",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/hw-free1.ovpn"
-},
-{
          "name": "Dtac-Lazada-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/lazada-vip1.ovpn"
 },
@@ -16,15 +8,6 @@
          "name": "Dtac-Lazada-FREE1",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-free2.ovpn"
 },
-{
-       "name": "Dtac-Line12Apps-VPS-VIP",
-         "url": "https://tak65.000webhostapp.com/line12apps-vip1.ovpn"
-      },
-
-{
-       "name": "Dtac-Line12Apps-FREE1",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12app2.ovpn"
-      },
 { 
 "name": "Dtac-NoPro-FREE1", 
 "url": "https://kguza.net/web/file/maine/Dtac..01.ovpn" 
@@ -75,7 +58,7 @@
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/true-nopro1.ovpn"
       }
  ],
-"message":"DTAC-HappyWork\nDTAC-Lazada\nDTAC-NoPro\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
+"message":"DTAC-Lazada\nDTAC-NoPro\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 84
+   "version": 85
 }
