a3a89263ced875e45d4263d14be91767e5e61061
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-01-31 17:25:45 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -8,40 +8,17 @@
          "name": "Dtac-Lazada-FREE1",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-free2.ovpn"
 },
-{ 
-"name": "Dtac-NoPro-FREE1", 
-"url": "https://kguza.net/web/file/maine/Dtac..01.ovpn" 
-}, 
-{ 
-"name": "Dtac-NoPro-FREE2", 
-"url": "https://kguza.net/web/file/maine/Dtac..02.ovpn" 
-}, 
-{ 
-"name": "Dtac-NoPro-FREE3", 
-"url": "https://kguza.net/web/file/maine/Dtac..03.ovpn" 
-}, 
-{ 
-"name": "Dtac-NoPro-FREE4", 
-"url": "https://kguza.net/web/file/maine/Dtac..04.ovpn" 
-}, 
-{ 
-"name": "Dtac-NoPro-FREE5", 
-"url": "https://kguza.net/web/file/maine/Dtac..05.ovpn" 
-}, 
-{ 
-"name": "Dtac-NoPro-FREE6", 
-"url": "https://kguza.net/web/file/maine/Dtac..06.ovpn" 
-}, 
-{ 
-"name": "Dtac-NoPro-FREE7", 
-"url": "https://kguza.net/web/file/maine/Dtac..07.ovpn" 
-},
 {
-       "name": "Dtac-NoPro-FREE8",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro2ovpn"
+       "name": "Dtac-Line12Apps-VPS-VIP",
+         "url": "https://tak65.000webhostapp.com/line12apps-vip1.ovpn"
+      },
+
+{
+       "name": "Dtac-Line12Apps-FREE1",
+         "url": "https://raw.githubusercontent.com/nds999/nds999/main/line12app2.ovpn"
       },
 {
-       "name": "Dtac-NoPro-FREE9",
+       "name": "Dtac-NoPro-FREE",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro.ovpn"
       },
 
@@ -58,7 +35,7 @@
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/true-nopro1.ovpn"
       }
  ],
-"message":"DTAC-Lazada\nDTAC-NoPro\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
+"message":"DTAC-HappyWork\nDTAC-Lazada\nDTAC-NoPro\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
    "version": 85
 }
