0c23382b23198d05b718f6691a9e26a66b1278de
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-02-19 21:00:59 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -37,7 +37,7 @@
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro2.ovpn"
       }
  ],
-"message":"🔰VPS Servers🔰\n1Month - 40Baht\n\nDTAC-Lazada\nDTAC-NoPro\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
+"message":"🔰VPS Servers🔰\n1Month - 40TB\n\nDTAC-Lazada\nDTAC-NoPro\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 98
+   "version": 99
 }
