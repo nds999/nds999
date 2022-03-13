6f2ffcb27d5cbce95b471dfa934a2f0e3eed76e7
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-03-13 21:56:47 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -4,10 +4,7 @@
          "name": "Dtac-Lazada-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/lazada-vip1.ovpn"
 },
-{
-         "name": "Dtac-Lazada-FREE",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/lazada-free2.ovpn"
-},
+
 {
        "name": "Dtac-Line12Apps-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/line12apps-vip1.ovpn"
@@ -17,31 +14,15 @@
          "url": "https://raw.githubusercontent.com/nds999/nds999/FNET-VPN/Newfile1.ovpn"
       },
 {
-       "name": "Dtac-NoPro-FREE2",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro1.ovpn"
-      },
-{
-       "name": "TH-Server-FREE",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro2.ovpn"
-      },
-{
        "name": "Dtac-NoPro-VPS-VIP",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro.vip.ovpn"
       },
 {
        "name": "True-NoPro-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/true-free1.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE1",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/true-nopro1.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE2",
-         "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro2.ovpn"
       }
  ],
 "message":"DTAC-Lazada\nDTAC-NoPro\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 104
+   "version": 105
 }
