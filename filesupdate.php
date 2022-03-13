047ddf51ca0319587760ccc538cd2e58f7261bab
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-03-14 04:01:37 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -10,8 +10,12 @@
          "url": "https://tak65.000webhostapp.com/line12apps-vip1.ovpn"
       },
 {
-       "name": "Dtac-NoPro-FREE1",
+       "name": "Dtac-NoPro-FREE",
          "url": "https://raw.githubusercontent.com/nds999/nds999/FNET-VPN/Newfile1.ovpn"
+      },
+{
+       "name": "DTAC-APN-Multimedia-NoPro-FREE",
+         "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro1.ovpn"
       },
 {
        "name": "Dtac-NoPro-VPS-VIP",
@@ -24,5 +28,5 @@
  ],
 "message":"DTAC-Lazada\nDTAC-NoPro\nDTAC-Line12Apps\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 105
+   "version": 106
 }
