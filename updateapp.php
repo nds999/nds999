f2a95081b1c3adf5eba96322b49ed971c4fa8257
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-03-25 03:13:58 +0700

    Update updateapp.php

--- updateapp.php
+++ updateapp.php
@@ -1,2 +1,2 @@
 { 
-"message":"Update Version 1.9\nUpdateကိုနှိပ်လိုက်ပါWebsiteသို့ဝင်ရောက်သွားရင်\n၁မိနစ်ခန့်သာသာစောင့်ဆိုင်းပေးပါ\nယခု 1.8 ဗားရှင်းသည် Update ယူရန် Website သို့ဝင်ရောက်ရန် စိတ်ရှည်ရန်လိုအပ်ပါသည်","title":" New app version has arrived ","version": 9 }
+"message":"Update Version 1.9\nUpdate Nowကိုႏွိပ္လိုက္ပါWebsiteသို႔ဝင္ေရာက္သြားရင္\n၁မိနစ္ခန့္သာသာေစာင့္ဆိုင္းေပးပါ\nယခု 1.8 ဗားရွင္းသည္ Update ယူရန္ Website သို႔ဝင္ေရာက္ရန္ စိတ္ရွည္ရန္လိုအပ္ပါသည္","title":" New app version has arrived ","version": 9 }
