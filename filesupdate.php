96809afb63f43f8c4fb038895e79e12b5b14cbda
Tint Aung Khaing <93828042+nds999@users.noreply.github.com>
2022-08-09 11:32:33 +0700

    Update filesupdate.php

--- filesupdate.php
+++ filesupdate.php
@@ -9,27 +9,15 @@
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro.vip.ovpn"
       },
 {
-       "name": "Dtac-NoPro-FREE",
-         "url": "https://raw.githubusercontent.com/Mcminmin/minmin.json/main/DTAC-1.ovpn"
-      },
-{
        "name": "DTAC-APN-VIP",
          "url": "https://raw.githubusercontent.com/nds999/nds999/main/dtac-nopro.vip.ovpn"
       },
 {
-       "name": "DTAC-APN-FREE",
-         "url": "https://raw.githubusercontent.com/Mcminmin/minmin.json/main/DTAC-1.ovpn"
-      },
-{
        "name": "True-NoPro-VPS-VIP",
          "url": "https://tak65.000webhostapp.com/true-free1.ovpn"
-      },
-{
-       "name": "True-NoPro-FREE",
-         "url": "https://raw.githubusercontent.com/Mcminmin/minmin.json/main/TRUE-9.ovpn"
       }
  ],
 "message":"DTAC-APN-Multimedia\nDTAC-Lazada\nDTAC-NoPro\nTRUE-NoPro\nQuick connection⚡\nThank you for using😊",
    "title":"New config update available",
-   "version": 130
+   "version": 131
 }
